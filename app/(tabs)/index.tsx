1ced7129db51979983a28b42d27ab54c937ffde7
Pius Bbaale <mayanjabbaale77@gmail.com>
2026-04-14 14:17:15 +0300

    features: project setup, nativewind, routing, tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,27 @@
-import "@/global.css"
-import { Text, View } from "react-native";
+import "@/global.css";
+import { Text } from "react-native";
 import { Link } from "expo-router";
- 
+import { styled } from 'nativewind';
+
+import {SafeAreaProvider, SafeAreaView as RNSafe } from "react-native-safe-area-context";
+
+const SafeAreaView = styled(RNSafe);
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-background">
-      <Text className="text-xl font-bold text-success">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onBoardingScreen" className="mt-4 p-4 bg-primary text-white rounded-lg">Go to OnBoarding</Link>
-      
-      <Link href="/(auth)/sign_in" className="mt-4 p-4 bg-primary text-white rounded-lg">Login</Link>
-      <Link href="/(auth)/signup" className="mt-4 p-4 bg-primary text-white rounded-lg">Sign Up</Link>
+    <SafeAreaProvider>
+          <SafeAreaView className="flex-1 bg-background p-5">
+            <Text className="text-xl font-bold text-success">
+              Welcome to Nativewind!
+            </Text>
+            <Link href="/onBoardingScreen" className="mt-4 p-4 bg-primary text-white rounded-lg">Go to OnBoarding</Link>
+            
+            <Link href="/(auth)/sign_in" className="mt-4 p-4 bg-primary text-white rounded-lg">Login</Link>
+            <Link href="/(auth)/signup" className="mt-4 p-4 bg-primary text-white rounded-lg">Sign Up</Link>
 
-      <Link  href={{pathname: "/(tabs)/subscriptions/[id]", params: {id: "claude"}}} className="mt-4 p-4 bg-primary text-white rounded-lg">Spotify</Link>
-    </View>
+            <Link  href="/(tabs)/subscriptions" className="mt-4 p-4 bg-primary text-white rounded-lg">Spotify</Link>
+          </SafeAreaView>
+    </SafeAreaProvider>
+
   );
 }
